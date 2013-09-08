5422aa9247c39462e4bd1335719ec9cd621ae16b
Nick Wright <tickthokk@gmail.com>
2013-09-08 16:25:11 -0400

    Analytics change

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -99,7 +99,7 @@
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			ga('create', 'UA-43830923-1', 'craftingasaservice.com/');
+			ga('create', 'UA-43830923-1', 'craftingasaservice.com');
 			ga('send', 'pageview');
 		</script>
 	</body>
