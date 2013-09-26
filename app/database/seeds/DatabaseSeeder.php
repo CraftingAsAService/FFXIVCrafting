1c2e453d93cb6aff67d38cdf24c4e51b0e7906ed
Nick Wright <tickthokk@gmail.com>
2013-09-25 21:53:25 -0400

    Debug info for seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,18 +10,25 @@ class DatabaseSeeder extends Seeder
 		// Don't bother logging queries
 		DB::connection()->disableQueryLog();
 
+		echo '** Stat Table **' . "\n";
 		$this->call('StatTableSeeder');
 
+		echo '** Jobs Table **' . "\n";
 		$this->call('JobTableSeeder');
 
+		echo '** Slot Table **' . "\n";
 		$this->call('SlotTableSeeder');
 
+		echo '** Data Table **' . "\n";
 		$this->call('DataTablesSeeder');
 
+		echo '** Quests Table **' . "\n";
 		$this->call('QuestSeeder');
 
+		echo '** Leve Table **' . "\n";
 		$this->call('LeveSeeder');
 
+		echo '** XP Table **' . "\n";
 		$this->call('XPSeeder');
 
 		echo "\n";
