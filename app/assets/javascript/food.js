3c5e0ca86c5eede44b4dd6ac0b92377190558494
Nick Wright <tickthokK@gmail.com>
2014-08-01 19:58:39 -0400

    #107 Food page redesigned

--- app/assets/javascript/food.js
+++ app/assets/javascript/food.js
@@ -1,12 +1,5 @@
 var food = {
 	init:function() {
-		$('.collapse').click(function() {
-			var el = $(this);
-
-			el.toggleClass('glyphicon-chevron-up').toggleClass('glyphicon-chevron-down');
-
-			el.closest('table').find('tbody').toggleClass('hidden');
-		});
 		
 		$('.vendors').on('click', function(event) {
 			event.preventDefault();
@@ -48,6 +41,205 @@ var food = {
 
 			return;
 		});
+
+		food.grid_events();
+
+		return;
+	},
+	grid_events:function() {
+		$('.food-grid td.reveal').click(function() {
+			var el = $(this),
+				a = el.data('a'),
+				b = el.data('b'),
+				fs = el.closest('.food-selection'),
+				items = fs.find('.items-table'),
+				tbody = items.find('tbody');
+
+			items.find('caption h2').html(el.data('originalTitle'));
+
+			var looking_for = a == b ? 1 : 2,
+				bonus = 0;
+
+			// if this cell is already active, don't do anything
+			if (el.hasClass('active'))
+				return;
+
+			// Make this cell "active"
+			$('.food-grid td.reveal').removeClass('active');
+			el.addClass('active');
+
+			// On pageload the items table is hidden
+			items.removeClass('hidden');
+
+			// Show all the applicable rows
+			$('tr', tbody)
+				.addClass('hidden')
+				.each(function() {
+					var el = $(this),
+						stats = el.data('stats').split('|'),
+						matched = 0;
+
+					if (stats.indexOf(a) != -1)
+						matched++;
+					if (a != b && stats.indexOf(b) != -1)
+						matched++;
+					if (stats.indexOf('Vitality') != -1)
+					{
+						bonus = 1;
+						matched++;
+					}
+
+					if (matched == looking_for + bonus && stats.length == looking_for + bonus)
+						el.removeClass('hidden');
+
+					return;
+				});
+
+			// Show/hide columns
+
+			looking_for += bonus;
+
+			$('.items-table th, .items-table td').removeClass('hidden');
+
+			if (looking_for < 3)
+				$('.items-table th:nth-child(4), .items-table td:nth-child(4)').addClass('hidden');
+			if (looking_for < 2)
+				$('.items-table th:nth-child(3), .items-table td:nth-child(3)').addClass('hidden');
+
+			// Prepare data for the HighChart
+			var series = [],
+				stat_a = '',
+				stat_b = '';
+
+			$('tr:visible', tbody).each(function() {
+				var el = $(this),
+					name = $('td:first-child a:last-child', el).text(),
+					a_td = $('td:nth-child(2)', el),
+					b_td = $('td:nth-child(3)', el);
+
+				if (b_td.length == 0)
+					b_td = a_td;
+
+				if (stat_a == '')
+					stat_a = a_td.data('statName');
+				if (stat_b == '')
+					stat_b = b_td.data('statName');
+
+				var quality = el.data('quality') == 'hq' ? ' (HQ)' : '';
+
+				series.push({
+					name: name + quality,
+					// color: 'rgb(37, 193, 193)', // Teal
+					color: 'rgb(255, 64, 64)', // Lobster
+					data: [[a_td.data('amount'), b_td.data('amount')]]
+				});
+
+				return;
+			});
+
+			// Load the HighChart
+			$('.highchart', fs).removeClass('hidden');
+			$('.highchart', fs).highcharts({
+				chart: {
+					type: 'scatter',
+					zoomType: 'xy'
+				},
+				title: {
+					text: el.data('originalTitle')
+				},
+				// subtitle: {
+				// 	text: 'Source: Heinz  2003'
+				// },
+				xAxis: {
+					title: {
+						enabled: true,
+						text: stat_a
+					},
+					startOnTick: true,
+					endOnTick: true,
+					showLastLabel: true
+				},
+				yAxis: {
+					title: {
+						text: stat_b
+					}
+				},
+				legend: {
+					enabled: false
+				},
+				plotOptions: {
+					scatter: {
+						marker: {
+							radius: 5,
+							states: {
+								hover: {
+									enabled: true,
+									lineColor: 'rgb(100,100,100)'
+								}
+							}
+						},
+						states: {
+							hover: {
+								marker: {
+									enabled: false
+								}
+							}
+						},
+						tooltip: {
+							headerFormat: '<b>{series.name}</b><br>',
+							pointFormat: '{point.x} ' + stat_a + ', {point.y} ' + stat_b
+						}
+					}
+				},
+				series: series
+			});
+
+			return;
+		});
+		
+		$('.items-table .sort').click(function() {
+			var el = $(this),
+				table = el.closest('table'),
+				tbody = table.find('tbody'),
+				rows = $('tr:visible', tbody);
+
+			// Switch the Sorting icons around
+			// Default them all back to "sort"
+			table.find('.sort .glyphicon')
+				.addClass('glyphicon-sort')
+				.removeClass('glyphicon-sort-by-attributes')
+				.removeClass('glyphicon-sort-by-attributes-alt');
+
+			// Are we sorting ascending or descending?
+			var order = el.data('order') == 'asc' ? 'desc' : 'asc';
+			el.data('order', order); // Save the order back in
+
+			// Change this icon
+			el.find('.glyphicon')
+				.removeClass('glyphicon-sort')
+				.addClass('glyphicon-sort-by-attributes' + (order == 'desc' ? '-alt' : ''));
+
+			var items = tbody.children('tr:visible').sort(function(a, b) {
+				var a = parseInt($(a).find('td:nth-child(' + el.data('column') + ')').data('amount')),
+					b = parseInt($(b).find('td:nth-child(' + el.data('column') + ')').data('amount'));
+				return (a < b) ? -1 : (a > b) ? 1 : 0;
+			});
+
+			if (order == 'desc')
+				// items is a nodelist, not an array
+				items = Array.prototype.reverse.call(items);
+
+			tbody.append(items);
+
+			return;
+		});
+
+		return;
+	},
+	sort_using_data:function(parent, child, selector)
+	{
+		
+		return;
 	}
 }
 
--- app/controllers/FoodController.php
+++ app/controllers/FoodController.php
@@ -10,53 +10,136 @@ class FoodController extends BaseController
 
 	public function getIndex()
 	{
-		// Items that are Food
-		$results = Item::with('name', 'baseparam', 'baseparam.name', 'vendors')
-			->where('itemcategory_id', 5)
-			->orderBy('id')
-			->get();
+		list($sections, $translations) = Cache::get('food_sections_' . Config::get('language'), function() {
+			// Items that are Food
+			$results = Item::with('name', 'baseparam', 'baseparam.name',  'baseparam.en_name', 'vendors')
+				->where('itemcategory_id', 5)
+				->orderBy('id')
+				->get();
 
-		// Group the food up
-		$food_groups = array();
-		foreach($results as $item)
-		{
-			$stats = array();
-			foreach ($item->baseparam as $baseparam)
+			// Group the food up
+			$food_groups = $translations = array();
+			foreach($results as $item)
 			{
-				$stats[$baseparam->name->term] = array(
-					'nq' => array(
-						'amount' => (int) $baseparam->pivot->nq_amount,
-						'limit' => $baseparam->pivot->nq_limit,
-						'threshold' => round($baseparam->pivot->nq_limit / ($baseparam->pivot->nq_amount / 100))
-					), 
-					'hq' => array(
-						'amount' => (int) $baseparam->pivot->hq_amount,
-						'limit' => $baseparam->pivot->hq_limit,
-						'threshold' => $baseparam->pivot->hq_amount == 0 ? 0 : round($baseparam->pivot->hq_limit / ($baseparam->pivot->hq_amount / 100))
-					)
+				$stats = $names = $key_name = array();
+				foreach ($item->baseparam as $baseparam)
+				{
+					$translations[$baseparam->en_name->term] = $baseparam->name->term;
+					$stats[$baseparam->en_name->term] = array(
+						'name' => $baseparam->name->term,
+						'nq' => array(
+							'amount' => (int) $baseparam->pivot->nq_amount,
+							'limit' => $baseparam->pivot->nq_limit,
+							'threshold' => round($baseparam->pivot->nq_limit / ($baseparam->pivot->nq_amount / 100))
+						), 
+						'hq' => array(
+							'amount' => (int) $baseparam->pivot->hq_amount,
+							'limit' => $baseparam->pivot->hq_limit,
+							'threshold' => $baseparam->pivot->hq_amount == 0 ? 0 : round($baseparam->pivot->hq_limit / ($baseparam->pivot->hq_amount / 100))
+						)
+					);
+				}
+
+				$names = array_keys($stats);
+				sort($names);
+
+				if (empty($names))
+					continue;
+
+				$food_groups[implode('|', $names)][] = array(
+					'id' => $item->id,
+					'has_hq' => $item->has_hq,
+					'name' => $item->name->term,
+					'min_price' => $item->min_price,
+					'vendor_count' => count($item->vendors),
+					'stats' => $stats
 				);
 			}
+			ksort($food_groups);
 
-			$names = array_keys($stats);
-			sort($names);
-
-			if (empty($names))
-				continue;
-
-			$food_groups[implode('|', $names)][] = array(
-				'id' => $item->id,
-				'name' => $item->name->term,
-				'min_price' => $item->min_price,
-				'vendor_count' => count($item->vendors),
-				'stats' => $stats
+			// Break these up into groups
+			$sections_base = array('data' => array(), 'headers' => array(), 'intersections' => array());
+			// This order determines how they show up on the page, so it's kind of important to keep it this way
+			$sections = array(
+				'Crafting' => $sections_base,
+				'Gathering' => $sections_base,
+				'Battle' => $sections_base,
+				'Resistances' => $sections_base
 			);
-		}
-		ksort($food_groups);
-		
-		#dd($food_groups);
+			foreach ($food_groups as $key => $value)
+			{
+				$keys = explode('|', $key);
+
+				$belongs_to = 'Battle';
+				
+				if (count(array_intersect(array('CP', 'Control', 'Craftsmanship'), $keys)) > 0)
+					$belongs_to = 'Crafting';
+				elseif (count(array_intersect(array('GP', 'Gathering', 'Perception'), $keys)) > 0)
+					$belongs_to = 'Gathering';
+				elseif (preg_match('/Resistance/', $key))
+					$belongs_to = 'Resistances';
+				
+				$sections[$belongs_to]['data'][$key] = $value;
+			}
+			
+			foreach ($sections as $section_key => $section_array)
+			{
+				$single_keys = array();
+				foreach (array_keys($section_array['data']) as $keys)
+					$single_keys = array_merge($single_keys, explode('|', $keys));
+				$single_keys = array_unique($single_keys);
+				sort($single_keys);
+				$sections[$section_key]['headers'] = $single_keys;
+
+				// Also get the Intersections
+				$intersections = array();
+				foreach ($single_keys as $i)
+				{
+					if ( ! isset($intersections[$i])) $intersections[$i] = array();
+					foreach ($single_keys as $j)
+					{
+						if ( ! isset($intersections[$i][$j])) $intersections[$i][$j] = 0;
+						$looking_for = $i == $j ? 1 : 2;
+
+						foreach ($section_array['data'] as $key => $value)
+						{
+							$keys = explode('|', $key);
+							$matched = 0;
+							$bonus = 0;
+
+							if (in_array($i, $keys))
+								$matched++;
+							if ($i != $j && in_array($j, $keys))
+								$matched++;
+							if (preg_match('/Vitality/', $key))
+							{
+								$bonus = 1;
+								$matched++;
+							}
+
+							if ($matched == $looking_for + $bonus && count($keys) == $looking_for + $bonus)
+							{
+								// Count base items
+								$intersections[$i][$j] += count($value);
+								// Count HQ items
+								foreach ($value as $x)
+									if ($x['has_hq'])
+										$intersections[$i][$j]++;
+							}
+						}
+					}
+				}
+				$sections[$section_key]['intersections'] = $intersections;
+			}
+
+			unset($food_groups);
+
+			return array($sections, $translations);
+		});
 
 		return View::make('pages.food')
-			->with('food_groups', $food_groups);
+			->with('sections', $sections)
+			->with('translations', $translations);
 	}
 
 	// private function food_stat_sort($a, $b)
