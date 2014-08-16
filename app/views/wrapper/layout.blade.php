24d9f0e68c44e075f4c3df2b45eae447f296b303
Nick Wright <tickthokK@gmail.com>
2014-08-16 05:03:25 -0400

    #16 Mobile Menu finished

--- app/views/wrapper/layout.blade.php
+++ app/views/wrapper/layout.blade.php
@@ -34,164 +34,235 @@
 	</head>
 	<body>
 
-		<div id="account">
-			<div class="container">
-				<ul class='hidden-xs'>
-					<li class='language-selector dropdown'>
-						<a href="#" class='dropdown-toggle' data-toggle='dropdown'>
-							<img src="/img/icons/flags/{{ Config::get('language') }}.png">
-							<span>{{ Config::get('site.full_languages')[Config::get('language')] }}</span>
-						</a>
-						<ul class='dropdown-menu' role='menu'>
-							@foreach(Config::get('site.full_languages') as $slug => $language)
-							<?php if ($slug == Config::get('language')) continue; ?>
-							<li>
-								<a tabindex='-1' href='http://{{ ($slug != 'en' ? $slug . '.' : '') . Config::get('language_base_url') }}'>
-									<img src="/img/icons/flags/{{ $slug }}.png"> {{ $language }}
-								</a>
-							</li>
-							@endforeach
-						</ul>
-					</li>
-					<li>
-						<a href="#"{{ isset($active) && $active == 'account' ? ' class="active"' : '' }}>
-							<img src="/img/icons/account.png">
-							<span>Account</span>
-						</a>
-					</li>
+		<input type='checkbox' id='nav-handler'>
+
+		{{-- MOBILE MENU ONLY --}}
+		<nav id='mobile-nav'>
+			<div class='header-bar'>
+				<div class="container">
+					<ul>
+						<li>
+							<label id='mobile-menu-button' for='nav-handler'>
+								Close Menu
+							</label>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="container navbox">
+				<ul class='nav navbar-nav'>
+
 					<li>
 						<a href="/list"{{ isset($active) && $active == 'list' ? ' class="active"' : '' }}>
 							<img src="/img/icons/bag.png">
 							<span>Crafting List</span>
 						</a>
 					</li>
-				</ul>
-				<ul class='visible-xs'>
+
 					<li>
-						<a href='#' class='mobile-menu'>
-							<img src="/img/reward.png" width='12' height='12'>
-							<span>Menu</span>
+						<a href="#"{{ isset($active) && $active == 'account' ? ' class="active"' : '' }}>
+							<img src="/img/icons/account.png">
+							<span>Account</span>
 						</a>
 					</li>
+					
+					<li>
+						<hr>
+					</li>
+
+					{{-- See /app/helpers.php for menu_item() function --}}
+					{{ menu_item('/',			'Home',			'home'		) }}
+					{{ menu_item('/equipment',	'Equipment',	'equipment'	) }}
+					{{ menu_item('/crafting',	'Crafting',		'crafting'	) }}
+					{{ menu_item('/career',		'Career',		'career'	) }}
+					{{ menu_item('/recipes',	'Recipe Book',	'recipes'	) }}
+					{{ menu_item('/quests',		'Quests',		'quests'	) }}
+					{{ menu_item('/leve',		'Leves',		'leves'		) }}
+					{{ menu_item('/stats',		'Stats',		'stats'		) }}
+					{{ menu_item('/materia',	'Materia',		'materia'	) }}
+					{{ menu_item('/food',		'Food',			'food'		) }}
+
+					<li>
+						<hr>
+					</li>
+
+					@foreach(Config::get('site.full_languages') as $slug => $language)
+					<?php if ($slug == Config::get('language')) continue; ?>
+					<li>
+						<a tabindex='-1' href='http://{{ ($slug != 'en' ? $slug . '.' : '') . Config::get('language_base_url') }}'>
+							<img src="/img/icons/flags/{{ $slug }}.png"> {{ $language }}
+						</a>
+					</li>
+					@endforeach
 				</ul>
+
 			</div>
-		</div>
-		<div id="header">
-			<div class='navbar'>
-				<div class='container'>
-					<div class="row">
-						<div class="col-xs-12 col-sm-4 col-lg-3 logo">
-							<!-- /f7f7f7/e2e2e2 -->
-							<img src="http://placehold.it/280x80" class="img-responsive" width='280' height='80'>
-						</div>
-						<div class="col-xs-12 col-sm-8 col-lg-9 menu-navbar">
-							<div class='navbar-header'>
-							</div>
-							<div class='collapse navbar-collapse'>
-								<ul class='nav navbar-nav'>
-									{{-- See /app/helpers.php for menu_item() function --}}
-									{{ menu_item('/',			'Home',			'home'		) }}
-									{{ menu_item('/equipment',	'Equipment',	'equipment'	) }}
-									{{ menu_item('/crafting',	'Crafting',		'crafting'	) }}
-									{{ menu_item('/career',		'Career',		'career'	) }}
-									{{ menu_item('/recipes',	'Recipe Book',	'recipes'	) }}
-									{{ menu_item('/quests',		'Quests',		'quests'	) }}
-									{{ menu_item('/leve',		'Leves',		'leves'		) }}
-									<li class='dropdown{{ isset($active) && in_array($active, array('stats', 'materia', 'food')) ? ' active' : '' }}'>
-										<a href='#' class='dropdown-toggle' data-toggle="dropdown">Resources <b class='caret'></b></a>
-										<ul class='dropdown-menu dropdown-menu-right'>
-											<li{{ isset($active) && $active == 'stats' ? ' class="active"' : '' }}><a href='/stats'>Stats</a></li>
-											<li{{ isset($active) && $active == 'materia' ? ' class="active"' : '' }}><a href='/materia'>Materia</a></li>
-											<li{{ isset($active) && $active == 'food' ? ' class="active"' : '' }}><a href='/food'>Food</a></li>
-										</ul>
+		</nav>
+
+		<main id='main-container'>
+			<header id='main-header'>
+				<div id="account">
+					<div class="container">
+						<ul class='hidden-xs'>
+							<li class='language-selector dropdown'>
+								<a href="#" class='dropdown-toggle' data-toggle='dropdown'>
+									<img src="/img/icons/flags/{{ Config::get('language') }}.png">
+									<span>{{ Config::get('site.full_languages')[Config::get('language')] }}</span>
+								</a>
+								<ul class='dropdown-menu' role='menu'>
+									@foreach(Config::get('site.full_languages') as $slug => $language)
+									<?php if ($slug == Config::get('language')) continue; ?>
+									<li>
+										<a tabindex='-1' href='http://{{ ($slug != 'en' ? $slug . '.' : '') . Config::get('language_base_url') }}'>
+											<img src="/img/icons/flags/{{ $slug }}.png"> {{ $language }}
+										</a>
 									</li>
+									@endforeach
 								</ul>
+							</li>
+							<li>
+								<a href="#"{{ isset($active) && $active == 'account' ? ' class="active"' : '' }}>
+									<img src="/img/icons/account.png">
+									<span>Account</span>
+								</a>
+							</li>
+							<li>
+								<a href="/list"{{ isset($active) && $active == 'list' ? ' class="active"' : '' }}>
+									<img src="/img/icons/bag.png">
+									<span>Crafting List</span>
+								</a>
+							</li>
+						</ul>
+						<ul class='visible-xs'>
+							<li>
+								<label id='mobile-menu-button' for='nav-handler'>
+									<img src="/img/reward.png" width='12' height='12'>
+									<span>Menu</span>
+								</label>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div id="header">
+					<div class='navbar'>
+						<div class='container'>
+							<div class="row">
+								<div class="col-xs-12 col-sm-4 col-lg-3 logo">
+									<!-- /f7f7f7/e2e2e2 -->
+									<img src="http://placehold.it/280x80" class="img-responsive" width='280' height='80'>
+								</div>
+								<div class="col-xs-12 col-sm-8 col-lg-9 menu-navbar">
+									<div class='navbar-header'>
+									</div>
+									<div class='collapse navbar-collapse'>
+										<ul class='nav navbar-nav'>
+											{{-- See /app/helpers.php for menu_item() function --}}
+											{{ menu_item('/',			'Home',			'home'		) }}
+											{{ menu_item('/equipment',	'Equipment',	'equipment'	) }}
+											{{ menu_item('/crafting',	'Crafting',		'crafting'	) }}
+											{{ menu_item('/career',		'Career',		'career'	) }}
+											{{ menu_item('/recipes',	'Recipe Book',	'recipes'	) }}
+											{{ menu_item('/quests',		'Quests',		'quests'	) }}
+											{{ menu_item('/leve',		'Leves',		'leves'		) }}
+											<li class='dropdown{{ isset($active) && in_array($active, array('stats', 'materia', 'food')) ? ' active' : '' }}'>
+												<a href='#' class='dropdown-toggle' data-toggle="dropdown">Resources <b class='caret'></b></a>
+												<ul class='dropdown-menu dropdown-menu-right'>
+													<li{{ isset($active) && $active == 'stats' ? ' class="active"' : '' }}><a href='/stats'>Stats</a></li>
+													<li{{ isset($active) && $active == 'materia' ? ' class="active"' : '' }}><a href='/materia'>Materia</a></li>
+													<li{{ isset($active) && $active == 'food' ? ' class="active"' : '' }}><a href='/food'>Food</a></li>
+												</ul>
+											</li>
+										</ul>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
+			</header>
 
-		<div id="banner">
-			<div class="container">
-				@yield('banner')
-			</div>
-		</div>
+			<section id='content-container'>
+				<div id="banner">
+					<div class="container">
+						@yield('banner')
+					</div>
+				</div>
 
-		<div id="content">
-			@yield('precontent')
+				<div id="content">
+					@yield('precontent')
 
-			<div class='container'>
+					<div class='container'>
 
-				@yield('content')
+						@yield('content')
 
-			</div>
-		</div>
+					</div>
+				</div>
 
-		<div id='footer'>
-			<div class='container'>
+				<div id='footer'>
+					<div class='container'>
 
-				<div class="row">
-					<div class="col-sm-3">
-						<p class="headline">Recent News</p>
+						<div class="row">
+							<div class="col-sm-3">
+								<p class="headline">Recent News</p>
 
-						<?php $wardrobe = new \Wardrobe\Core\Repositories\DbPostRepository(); ?>
-						@foreach($wardrobe->active(3) as $post)
-							<div class="post">
-								<div class="title">
-									<a href="/blog/post/{{{ $post->slug }}}">{{ $post->title }}</a>
-								</div>
-								<div class="date">
-									<img src="/img/icons/time.png"><span>{{ date("M d, Y", strtotime($post->publish_date)) }}</span>
-									<?php /*by <span class='who'>{{ $post->user->first_name }} {{ $post->user->last_name }}</span> */ ?>
-								</div>
-								<hr>
+								<?php $wardrobe = new \Wardrobe\Core\Repositories\DbPostRepository(); ?>
+								@foreach($wardrobe->active(3) as $post)
+									<div class="post">
+										<div class="title">
+											<a href="/blog/post/{{{ $post->slug }}}">{{ $post->title }}</a>
+										</div>
+										<div class="date">
+											<img src="/img/icons/time.png"><span>{{ date("M d, Y", strtotime($post->publish_date)) }}</span>
+											<?php /*by <span class='who'>{{ $post->user->first_name }} {{ $post->user->last_name }}</span> */ ?>
+										</div>
+										<hr>
+									</div>
+								@endforeach
+
+								<p class="view-all"><a href="/blog/">View All Recent News</a></p>
 							</div>
-						@endforeach
+							<div class="col-sm-3">
+								<p class="headline">Current Patch</p>
+								<img src="/img/current_patch.png" class="img-responsive">
+								<p>This site has been optimzed for Patch 2.3 Defenders of Eorzea</p>
+							</div>
+							<div class="col-sm-3">
+								<p class="headline">Donations</p>
+								<p>I've spent more time building this site than actually playing. Help me relax and show my wife this isn't just a hobby!</p>
+								<p class="view-all"><a href="#buymeabeer">Donate Today!</a></p>
+							</div>
+							<div class="col-sm-3">
+								<p class="headline">Other Links</p>
 
-						<p class="view-all"><a href="/blog/">View All Recent News</a></p>
-					</div>
-					<div class="col-sm-3">
-						<p class="headline">Current Patch</p>
-						<img src="/img/current_patch.png" class="img-responsive">
-						<p>This site has been optimzed for Patch 2.3 Defenders of Eorzea</p>
-					</div>
-					<div class="col-sm-3">
-						<p class="headline">Donations</p>
-						<p>I've spent more time building this site than actually playing. Help me relax and show my wife this isn't just a hobby!</p>
-						<p class="view-all"><a href="#buymeabeer">Donate Today!</a></p>
-					</div>
-					<div class="col-sm-3">
-						<p class="headline">Other Links</p>
-
-						<p><a href="mailto:tickthokk@gmail.com">Contact Me</a></p>
-						<hr>
-						<p><a href="/report">Report a bug</a></p>
-						<hr>
-						<p><a href="http://na.finalfantasyxiv.com/lodestone/character/2859264/">My Character</a></p>
-						<hr>
-						<p><a href="http://ffxivclock.com/">FFXIV Clock</a></p>
-						<hr>
-						<p><a href="http://www.reddit.com/r/ffxivcrafting">Subreddit</a></p>
-						<hr>
-						<p><a href="/credits">Source Credits &amp; Resources</a></p>
+								<p><a href="mailto:tickthokk@gmail.com">Contact Me</a></p>
+								<hr>
+								<p><a href="/report">Report a bug</a></p>
+								<hr>
+								<p><a href="http://na.finalfantasyxiv.com/lodestone/character/2859264/">My Character</a></p>
+								<hr>
+								<p><a href="http://ffxivclock.com/">FFXIV Clock</a></p>
+								<hr>
+								<p><a href="http://www.reddit.com/r/ffxivcrafting">Subreddit</a></p>
+								<hr>
+								<p><a href="/credits">Source Credits &amp; Resources</a></p>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-		<div id="copyright-info">
-			<div class="container">
-				<div class="row">
-					<div class="col-xs-12 col-sm-8">
-						2014 FFXIV - Crafting as a Service. FINAL FANTASY is a registered trademark of Square Enix Holdings Co., Ltd.
-					</div>
-					<div class="col-xs-12 col-sm-4 text-right">
-						<a href="#">Back To Top<span class="glyphicon glyphicon-chevron-up"></span></a>
+				<div id="copyright-info">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12 col-sm-8">
+								2014 FFXIV - Crafting as a Service. FINAL FANTASY is a registered trademark of Square Enix Holdings Co., Ltd.
+							</div>
+							<div class="col-xs-12 col-sm-4 text-right">
+								<a href="#">Back To Top<span class="glyphicon glyphicon-chevron-up"></span></a>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
+			</section>
+		</main>
 
 		<div id='notifications'></div>
 
