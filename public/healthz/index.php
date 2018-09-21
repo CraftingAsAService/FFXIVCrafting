5a7bf0cf391df60c2c5ce21ca38351704986f06b
Nick Wright <tickthokk@gmail.com>
2018-09-21 13:23:22 -0400

    Echo OK if DB Connects

--- public/healthz/index.php
+++ public/healthz/index.php
@@ -1,2 +1,14 @@
 <?php
-	echo 'OK';
+	$envs = [];
+	foreach (explode("\n", file_get_contents('../../.env')) as $row)
+	{
+		list($key, $value) = explode('=', $row);
+		$envs[$key] = $value;
+	}
+	unset($row);
+
+	$pdo = new PDO('mysql:host=' . $envs['DB_HOST'] . ';dbname=' . $envs['DB_DATABASE'], $envs['DB_USERNAME'] ?? '', $envs['DB_PASSWORD'] ?? '');
+	$result = $pdo->query('SELECT DISTINCT `id` FROM `item` LIMIT 1')->fetch();
+
+	if (isset($result['id']) && is_numeric($result['id']))
+		echo 'OK';
