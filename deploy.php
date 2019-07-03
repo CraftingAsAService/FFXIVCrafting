b8e506d2c8b6f857cff48159d9961da2d4dd8c29
Nick Wright <tickthokk@gmail.com>
2019-07-03 15:12:51 -0400

    :rocket: Deploy script updates

--- deploy.php
+++ deploy.php
@@ -4,6 +4,9 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
+// Server user set in ~/.aliases
+$user = exec('echo $DOUSER');
+
 // Project name
 set('application', 'ffxivcrafting');
 set('allow_anonymous_stats', true);
@@ -11,52 +14,127 @@ set('allow_anonymous_stats', true);
 // Project repository
 set('repository', 'git@github.com:CraftingAsAService/FFXIVCrafting.git');
 
+set('default_stage', 'production');
+
 set('git_tty', true);
-set('git_cache', false);
-// set('git_clone_dissociate', false); // TESTING
+set('git_cache', false); // Seems to be faster without it
 
-// Preserve shared/writable/copy dirs & files from the
-//  laravel recipe with array_merge
-set('shared_dirs', array_merge(get('shared_dirs'), [
+set('writable_mode', 'chgrp');
+set('http_user', 'www-data');
+set('http_group', 'www-data');
+
+set('shared_dirs', [
+	'storage',
 	'public/assets',
-]));
+]);
 
-// set('shared_files', array_merge(get('shared_files'), [
-// ]));
+set('shared_files', [
+	'.env',
+]);
 
-// set('writable_dirs', array_merge(get('writable_dirs'), [
-// ]));
+set('copy_dirs', [
+	'vendor',
+]);
 
-set('copy_dirs', array_merge(get('copy_dirs'), [
-	'vendor'
-]));
+set('writable_dirs', [
+	'bootstrap/cache',
+	'storage',
+	'storage/app',
+	'storage/app/public',
+	'storage/framework',
+	'storage/framework/cache',
+	'storage/framework/sessions',
+	'storage/framework/views',
+	'storage/logs',
+]);
 
 // Hosts
 
 host('ultros')
-	->user('$DOUSER') // Server user setup in ~/.aliases
+	->user($user)
 	->forwardAgent() // Use local ssh credentials for git
 	->stage('production')
-	->set('http_user', 'www-data')
-    ->set('deploy_path', '/srv/test/{{application}}');
+	->set('branch', function () {
+		// Default to master, but allow overrides by using --branch
+		return input()->getOption('branch') ?: 'master';
+	})
+	->set('deploy_path', '/srv/test/{{application}}');
 
 // Tasks
 
-desc('ENV File Setup');
-task('environment', function() {
-	run('rsync -v -e ssh .env.{{stage}} $(echo $DOUSER)@{{hostname}}:{{deploy_path}}/shared/.env');
+desc('Upload the database');
+task('upload:db', function() {
+	upload('../ultros/caas.sql', '~/caas.sql');
+});
+
+desc('Update the database on Ultros');
+task('ultros:db', function() {
+	require __DIR__ . '/vendor/autoload.php';
+	$dotenv = \Dotenv\Dotenv::create(__DIR__, '.env.' . get('stage'));
+	$dotenv->load();
+	run('mysql --defaults-file=~/password.cnf -u ' . getenv('DB_USERNAME') . ' -h ' . getenv('DB_HOST') . ' ' . getenv('DB_DATABASE') . ' < ~/caas.sql');
+});
+before('ultros:db', 'upload:db');
+
+desc('Update the assets on Ultros');
+task('ultros:assets', function() {
+	upload('../assets/ffxiv/i/', '/srv/www/assets/i/');
+});
+
+desc('Upload all env files');
+task('upload:envs', function() {
+	// upload('.env.local', '/srv/.envs/.env.local');
+	// upload('.env.dev', '/srv/.envs/.env.dev');
+	// upload('.env.qa', '/srv/.envs/.env.qa');
+	upload('.env.production', '/srv/.envs/.env.production');
+});
+
+desc('Download all env files');
+task('download:envs', function() {
+	// download('/srv/.envs/.env.local', '.env.local');
+	// download('/srv/.envs/.env.dev', '.env.dev');
+	// download('/srv/.envs/.env.qa', '.env.qa');
+	download('/srv/.envs/.env.production', '.env.production');
+});
+
+desc('Upload env file');
+task('upload:env', function() {
+	upload('.env.{{stage}}', '{{deploy_path}}/shared/.env');
+});
+
+desc('Download env file');
+task('download:env', function() {
+	download('{{deploy_path}}/shared/.env', '.env.{{stage}}');
 });
 
 desc('Execute artisan refresh');
 task('artisan:refresh', function () {
-    run('{{bin/php}} {{release_path}}/artisan refresh');
+	run('{{bin/php}} {{release_path}}/artisan refresh');
 });
 
-// Before deploying vendors, copy the vendor folder over
-before('deploy:vendors', 'deploy:copy_dirs');
+// Primary Task
+desc('Deploy your project');
+task('deploy', [
+	'deploy:info',
+	'deploy:prepare',
+	'deploy:lock',
+	'deploy:release',     // Prepare `release`
+	'deploy:update_code', // GitHub call
+	'deploy:shared',      // Create symlinks for shared_dirs and files
+	'deploy:copy_dirs',   // Copies vendor/ folder
+	'deploy:vendors',     // composer install --no-dev -o
+	'deploy:writable',    // Makes sure storage folders are writable
+	'artisan:refresh',    // artisan:optimize on steroids
+	'artisan:migrate',    // Database migration
+	'deploy:symlink',     // `release` is now `current`
+	'deploy:unlock',
+	'cleanup',
+]);
 
+// Additional task executions
+
+// If the deployment fails, unlock it for future deployments
 after('deploy:failed', 'deploy:unlock');
-before('deploy:symlink', 'artisan:migrate');
 
-before('artisan:optimize', 'environment');
-before('artisan:optimize', 'artisan:refresh');
+// Create a storage/app/public symlink
+// after('deploy:shared', 'artisan:storage:link');
--- readme.md
+++ readme.md
@@ -9,9 +9,15 @@ Image for Footer: http://na.finalfantasyxiv.com/lodestone/special/patchnote_log/
 ## Updating
 
 ```
-a cache:clear file (optionally necessary)
-a aspir:data
-a aspir:assets
-a aspir:migrate
-a build
+php artisan cache:clear file (optionally necessary)
+php artisan aspir:data
+php artisan aspir:migrate
+
+php artisan build
+dep ultros:db
+
+php artisan aspir:assets
+dep ultros:assets
+
+dep deploy
 ```
