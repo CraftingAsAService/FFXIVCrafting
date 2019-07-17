bef7a8f7e242bed94e611b7c39a0b39324eb5615
Nick Wright <tickthokk@gmail.com>
2019-07-17 14:23:22 -0400

    :zap: Disabled web request monitoring

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends HttpKernel {
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Inspector\Laravel\Middleware\WebRequestMonitoring::class,
+            // \Inspector\Laravel\Middleware\WebRequestMonitoring::class,
         ],
 
         'api' => [
