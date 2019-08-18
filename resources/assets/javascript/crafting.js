ff031ad7c8db5d515e69558d437d4fda27896918
Nick Wright <tickthokk@gmail.com>
2019-08-18 19:51:45 -0400

    :bug: Name on export had too much whitespace

--- resources/assets/javascript/crafting.js
+++ resources/assets/javascript/crafting.js
@@ -100,7 +100,7 @@ var crafting = {
 				var row = [],
 					el = $(this);
 
-				row.push(el.find('a.name').text());
+				row.push($.trim(el.find('a.name').text()));
 				row.push(el.data('ilvl') || el.find('.ilvl').text().replace(/\s|\n/ig, '') || '-');
 				row.push(el.data('yields'));
 
