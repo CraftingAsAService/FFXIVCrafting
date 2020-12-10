50234cda703e8911ae02d9a820e52fa8ae8549f1
Nick Wright <tickthokk@gmail.com>
2020-12-10 13:27:56 -0500

    :arrow_up: 5.4 Release

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -247,8 +247,8 @@
 							</div>
 							<div class="col-sm-3">
 								<p class="headline">Current Patch</p>
-								<img src="/img/patch/5.3.png" class="img-responsive">
-								<p>This site has been optimized for Patch 5.3</p>
+								<img src="/img/patch/5.4.png" class="img-responsive">
+								<p class='text-center'>This site has been optimized for Patch 5.4</p>
 							</div>
 							<div class="col-sm-3">
 								<p class="headline">Support</p>
