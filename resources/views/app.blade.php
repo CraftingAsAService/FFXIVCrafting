f13781a6938e71537cb9d7f524305d5675fb58b3
Nick Wright <tickthokk@gmail.com>
2021-05-03 07:09:31 -0400

    :art: 5.5 update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -244,8 +244,8 @@
 							</div>
 							<div class="col-sm-3">
 								<p class="headline">Current Patch</p>
-								<img src="/img/patch/5.4.png" class="img-responsive">
-								<p class='text-center'>This site has been optimized for Patch 5.4</p>
+								<img src="/img/patch/5.5.png" class="img-responsive">
+								<p class='text-center'>This site has been optimized for Patch 5.5</p>
 							</div>
 							<div class="col-sm-3">
 								<p class="headline">Support</p>
